1398e4997123956153876616f5828f1e6945fddc
suphatchaklintein <56069265+suphatchaklintein@users.noreply.github.com>
2019-10-02 18:50:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,17 @@ def callback():
     else ;
          print('ว่าอะไรค่ะ)
     return '',200
+def sendText(user, text):
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
+  headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
+  }
+  data = json.dumps({
+    "replyToken":user,
+    "messages":[{"type":"text","text":text}]
+  })
+  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+
+if __name__ == '__main__':
+    app.run()
